59f907fa11f713194bf1b6345fa7accd3a046c7f
JeremyLu607 <Jeremy.Lu@warwick.ac.uk>
2020-07-18 17:12:10 +0800

    'all'

---
+++ app.js
@@ -0,0 +1,63 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// session 配置
+app.use(session({
+  secret: 'project',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 250000000000000 } // 登陆有效时长直到9999年
+}))
+
+// 登录拦截
+app.get('*', function(req, res, next) {
+  var username = req.session.username
+  var path = req.path
+  console.log('session', username)
+  if (path != '/login' && path != '/regist') {
+    if (!username) {
+      res.redirect('/login')
+    }
+  }
+  next()
+})
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
